fb9bdcedce19fed55e4db1b7a009b3cdef3a8662
David <crusedmoss@gmail.com>
2019-05-15 01:19:13 -0500

    basic navbar

--- src/App.js
+++ src/App.js
@@ -9,18 +9,26 @@ function App() {
         <div className="uk-navbar-left">
 
             <ul className="uk-navbar-nav">
-                <li className="uk-active"><a href="#">Active</a></li>
+                <li className="uk-active"><a href="#">Fixterstore</a></li>
+                <li><a href="#">Catalogo</a></li>
+            </ul>
+
+        </div>
+
+        <div className="uk-navbar-right">
+
+            <ul className="uk-navbar-nav">
+                <li className="uk-active"><a href="#">Iniciar sesión</a></li>
                 <li>
-                    <a href="#">Parent</a>
-                    <div className="uk-navbar-dropdown">
-                        <ul className="uk-nav uk-navbar-dropdown-nav">
-                            <li className="uk-active"><a href="#">Active</a></li>
-                            <li><a href="#">Item</a></li>
-                            <li><a href="#">Item</a></li>
+                    <a href="#">David Zavala</a>
+                    <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li class="uk-active"><a href="#">Perfil</a></li>
+                            <li><a href="#">Nuevo producto</a></li>
+                            <li><a href="#">Cerrar sesión</a></li>
                         </ul>
                     </div>
                 </li>
-                <li><a href="#">Item</a></li>
             </ul>
 
         </div>
