f8869fdb638baae45e71ccdcb2f8acf09617eb12
Héctor BlisS <contacto@fixter.org>
2019-05-20 20:43:52 -0500

    reducers everywhere, wait reduce is a reducer wat??

--- src/redux/ducks/cart.js
+++ src/redux/ducks/cart.js
@@ -14,8 +14,8 @@ let initialState = {
 export default function reducer(state=initialState, action){
     switch(action.type){
         case ADD_TO_CART:
-            let {total,quantity,product} = action.payload
-            return {...state, total,quantity,items:[product, ...state.items]}
+            let {total,quantity,items} = action.payload
+            return {...state, total,quantity,items}
         case UPDATE_ITEM:
         case REMOVE_ITEM:
         case EMPTY_CART:
@@ -30,9 +30,22 @@ let addToCartSuccess = (payload) => ({
 })
 
 export let addToCart = (product) => (dispatch, getState) => {
+     // si es el mismo producto solo sumamos las cantidades
     let cart = getState().cart
-    let total = cart.total + product.price
-    let quantity = cart.quantity + 1
-    dispatch(addToCartSuccess({product,total,quantity})) 
+    let p = cart.items.find(p=>p.id === product.id)
+    let items = []
+    if(p){
+        product.quantity = p.quantity ? ++p.quantity : 2
+        items = cart.items.map(p=>{
+            if(p.id===product.id) return product
+            return p
+        })
+    }else{
+        product.quantity = 1
+        items = [...cart.items, product]
+    }
+    let total = items.reduce((acc,p)=>acc+(p.price *p.quantity) ,0)
+    let quantity = items.reduce((acc,p)=>acc+p.quantity,0)
+    dispatch(addToCartSuccess({items,total,quantity})) 
 }
 
